6bd21120a6f336b9d0cd8c0271c456dbd2344cf4
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:28 +0300

    Первый способ первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = numberA;
+int min = numberB;
+if(numberA==numberB)
+{
+Console.WriteLine("Числа равны");
+}
+else
+{
+ if (numberB>max)
+ {
+ max=numberB;
+ min=numberA;
+ }
+ else
+ {
+ max=numberA;
+ min=numberB;
+ }
+}
+Console.Write("max:");
+Console.WriteLine(max);
+Console.Write("min:");
+Console.WriteLine(min);
