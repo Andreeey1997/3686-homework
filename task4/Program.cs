4cd17b96c74c7edba0ef348cf2826fcba091c9b8
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:28 +0300

    Решение четвертой задачи

---
+++ task4/Program.cs
@@ -0,0 +1,18 @@
+﻿int MAX (int numberA, int numberB, int numberC)
+{
+int result = numberA;
+if (numberB>result) result = numberB;
+if (numberC>result) result = numberC;
+return result;
+}
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = MAX(numberA, numberB, numberC);
+Console.Write("max:");
+Console.WriteLine(max);
