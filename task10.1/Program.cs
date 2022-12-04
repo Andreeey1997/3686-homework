17150fef543f63d5c7b36a669fe0cbe3353c46e7
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:29 +0300

    Решение задачи 10 первый способ

---
+++ task10.1/Program.cs
@@ -0,0 +1,13 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+
+if (number/100 > 0)
+{
+int LastTwoDigits = number%100;
+int SecondDigit = LastTwoDigits/10;
+Console.WriteLine(SecondDigit);
+}
+else
+{
+Console.WriteLine("Введите трехзначное число");
+}
