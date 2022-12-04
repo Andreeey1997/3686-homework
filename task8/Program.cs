ef46c705c58cf844bb7dd7c1e261c8ee92236dc6
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:28 +0300

    Решение восьмой задачи

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+int result = i%2;
+if (result == 0)
+ {
+ Console.WriteLine(i);
+ }
+}
