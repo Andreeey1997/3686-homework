c2ea063bcf5415ff4f71c62f6ff9ada6d8c5b2d2
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:28 +0300

    Решение шестой задачи

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number%2;
+
+if (result==0)
+{
+Console.WriteLine("Число четное");
+}
+else
+{
+Console.WriteLine("Число нечетное");
+}
