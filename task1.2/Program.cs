3c42d53f33fc5fb167d4ac53149c87744a2f5417
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:28 +0300

    Второй способ первой задачи

---
+++ task1.2/Program.cs
@@ -0,0 +1,26 @@
+﻿int MAX(int numberA, int numberB)
+{
+int result = numberA;
+if (numberB>result) result = numberB;
+return result;
+}
+
+int MIN(int numberA, int numberB)
+{
+int result = numberA;
+if (numberB < result) result = numberB;
+return result;
+}
+
+Console.WriteLine("Введите первое число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int max = MAX(numberA, numberB);
+int min = MIN(numberA, numberB);
+
+Console.Write("max:");
+Console.WriteLine(max);
+Console.Write("min:");
+Console.WriteLine(min);
