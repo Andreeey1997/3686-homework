7da7da4cfbf1a439ee9ed75ed791eff50f8f5f0e
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:29 +0300

    Решение десятой задачи второй способ

---
+++ task10.2/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(100,1000);
+Console.WriteLine(number);
+
+if (number/100>0)
+{
+int SecondDigit = (number%100)/10;
+Console.WriteLine(SecondDigit);
+}
+else
+{
+Console.WriteLine("Введите трехзначное число");
+}
