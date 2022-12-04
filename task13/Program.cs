430830f4e4c5092c057e455505da08cd50d8613d
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:29 +0300

    Решение тринадцатой задачи

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+if (number < 100)
+{
+Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+ while (number >= 1000)
+ {
+ number=number/10;
+ }
+ result = number%10;
+ Console.WriteLine(result);
+}
