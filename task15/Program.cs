68dcef59dc0a73c6d520d6aefa43f9c2b72bbce8
Andreeey1997 <sem.romancov@mail.ru>
2022-12-04 17:44:29 +0300

    Решение пятнадцатой задачи

---
+++ task15/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 1 && number <=7)
+{
+ if (number == 6 || number == 7)
+ {
+ Console.WriteLine("Да, это выходной");
+ }
+ else
+ {
+ Console.WriteLine("Нет, это не выходной");
+ }
+}
+else
+{
+Console.WriteLine("Такого дня недели не существует");
+}
