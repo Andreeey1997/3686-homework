78d904286c80d3b7521424814d6582096eac071e
Andreeey1997 <sem.romancov@mail.ru>
2022-12-11 16:43:46 +0300

    Решение 25 задачи

---
+++ task25/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число, которое нужно возвести в степень: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите целую положительную степень: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int AinExtentB = 1;
+
+for (int i = 1; i <= B; i++)
+{
+AinExtentB = AinExtentB * A;
+}
+
+Console.WriteLine ($"{A}^{B} это {AinExtentB}");
