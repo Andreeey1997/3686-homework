ae9a9fe3688fc52eb329cdda6c40d2a1cadb77cb
Andreeey1997 <sem.romancov@mail.ru>
2022-12-11 16:33:28 +0300

    Решение 19 задачи

---
+++ task19/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число, состоящее из 5 цифр: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int NewNumber = 0;
+
+int temp = Number;
+
+while (temp > 0)
+{
+NewNumber = NewNumber*10 + temp%10;
+temp = temp/10;
+}
+
+if (NewNumber == Number)
+{
+Console.WriteLine("Да, палиндром");
+}
+else
+{
+Console.WriteLine("Нет, не палиндром");
+}
