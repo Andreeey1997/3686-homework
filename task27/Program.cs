ee3fa4c7cf5a009b897b6623efb67b0babba34b8
Andreeey1997 <sem.romancov@mail.ru>
2022-12-11 16:36:14 +0300

    Решение 27 задачи

---
+++ task27/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadInt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumOfDigits(int number)
+{
+ int sum = 0;
+ while (number > 0)
+ {
+ sum = sum + number%10;
+ number /=10;
+ }
+ return sum;
+}
+
+int number = ReadInt("Введите число");
+int result = GetSumOfDigits(number);
+Console.WriteLine(result);
