d7aa6bfcb5a3ebe8408260d826494ee8487080d9
Andreeey1997 <sem.romancov@mail.ru>
2022-12-11 16:50:25 +0300

    Решение 29 задачи

---
+++ task29/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int[8];
+
+int ReadInt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i < 8; i++)
+{
+array[i]=ReadInt("Введите элемент массива");
+}
+
+Console.WriteLine(string.Join(", ", array));
