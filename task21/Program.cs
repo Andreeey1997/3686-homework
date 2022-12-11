26af447ac594ae0cbcf59afc37a3a812175d02c8
Andreeey1997 <sem.romancov@mail.ru>
2022-12-11 16:16:15 +0300

    Решение 21 задачи

---
+++ task21/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите координаты X(1)");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y(1)");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z(1)");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты X(2)");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Y(2)");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Z(2)");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2));
+Console.WriteLine($"Итог:{res}");
