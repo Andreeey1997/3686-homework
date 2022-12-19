9acb2269b06689f95ccd50a47a60521bae7c0393
Andreeey1997 <sem.romancov@mail.ru>
2022-12-19 21:14:09 +0300

    Решение 34 задачи

---
+++ task34/Program.cs
@@ -0,0 +1,22 @@
+﻿Random rand = new Random();
+void GetRandomArray(int[] array)
+{
+for (int i = 0; i<array.Length; i++)
+ {
+ array[i] = rand.Next(100,1000);
+ }
+}
+
+int[] array = new int[10];
+GetRandomArray(array);
+Console.WriteLine($"{string.Join(",", array)}");
+
+int count = 0;
+for (int i=0; i<array.Length; i++)
+{
+ if (array[i]%2==0)
+ {
+ count+=1;
+ }
+}
+Console.WriteLine($"Количество четных элементов массива: {count}");
