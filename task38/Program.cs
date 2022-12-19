d76a7e7a185128f9c8a9e3d91319e12118413a34
Andreeey1997 <sem.romancov@mail.ru>
2022-12-19 21:34:38 +0300

    Решение задачи 38

---
+++ task38/Program.cs
@@ -0,0 +1,41 @@
+﻿Random rand = new Random();
+
+void GetRandomArray(int [] array)
+{
+for (int i = 0; i<array.Length; i++)
+ {
+ array[i] = rand.Next(100,1000);
+ }
+}
+
+
+int[] array = new int[10];
+GetRandomArray(array);
+
+Console.WriteLine($"{string.Join(",", array)}");
+
+int MAX(int [] array)
+{
+int result = array[0];
+for (int i=0; i<array.Length; i++)
+{
+ if (array[i]>result) result = array[i];
+ 
+}
+return result;
+}
+
+int MIN (int [] array)
+{
+int result = array[0];
+for (int i=0; i<array.Length; i++)
+{
+ if (array[i]<result) result = array[i];
+}
+return result;
+}
+
+int minimum = MIN(array);
+int maximum = MAX(array);
+int NewResult = maximum - minimum;
+Console.WriteLine(NewResult);
