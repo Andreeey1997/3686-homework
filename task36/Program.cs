9dd1ad2fad64bd7c5a84d9b305cb3e85a196e050
Andreeey1997 <sem.romancov@mail.ru>
2022-12-19 21:18:07 +0300

    Решение 36 задачи

---
+++ task36/Program.cs
@@ -0,0 +1,23 @@
+﻿Random rand = new Random();
+
+void GetRandomArray(int [] array)
+{
+for (int i = 0; i<array.Length; i++)
+ {
+ array[i] = rand.Next(100,1000);
+ }
+}
+
+int[] array = new int[10];
+GetRandomArray(array);
+Console.WriteLine($"{string.Join(",", array)}");
+
+int sum = 0;
+for (int i=0; i<array.Length; i++)
+{
+ if (i%2 != 0)
+ {
+ sum = sum + array[i];
+ }
+}
+Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sum}");
