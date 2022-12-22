6477f9e77e0841fc87dc99711b7b0f3c19f951f6
Andreeey1997 <sem.romancov@mail.ru>
2022-12-22 12:12:34 +0300

    Решение 41 задачи

---
+++ task41/Program.cs
@@ -0,0 +1,32 @@
+﻿int PositiveCount()
+{
+    string MessageText = "0";
+    int MasLength = 1;
+    int[] PreviousMas = new int[0];
+    int positive = 0;
+
+    for(int i = 0; i < MasLength; i++)
+    {
+        Console.WriteLine("Введите число");
+        MessageText = Console.ReadLine();
+        if (MessageText != "stop")
+        {
+            int message = Convert.ToInt32(MessageText);
+            int[] array = new int [MasLength];
+            for(int j = 0; j < MasLength - 1; j++)
+            {
+                array[j] = PreviousMas[j];
+            }
+            if (message > 0)
+            {
+                positive += 1;
+            }
+            array[MasLength - 1] = message;
+            PreviousMas = array;
+            MasLength += 1;
+        }
+    }
+    return positive;
+}
+
+Console.WriteLine($"Количество положительных чисел = {PositiveCount()}");
