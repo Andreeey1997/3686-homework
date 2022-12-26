54f0817a12f46e8a63527d9248349dcd741c5873
Andreeey1997 <sem.romancov@mail.ru>
2022-12-26 23:06:25 +0300

    Решение 52 задачи

---
+++ task52/Program.cs
@@ -0,0 +1,52 @@
+﻿var rand = new Random();
+
+int ReadInt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+double [,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+double [,] matrix = new double [rows, columns];
+ for (int i = 0; i<matrix.GetLength(0); i++)
+ {
+  for (int j = 0; j<matrix.GetLength(1); j++)
+  {
+  matrix[i,j]=rand.Next(leftRange, rightRange+1);
+  }
+ }
+ return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+ for (int i = 0; i<matrix.GetLength(0); i++)
+ {
+  for (int j = 0; j<matrix.GetLength(1); j++)
+  {
+  Console.Write(matrix[i,j]+ " ");
+  }
+  Console.WriteLine();
+ }
+}
+
+int rowsCount=ReadInt("Введите число строк:");
+int columnsCount=ReadInt("Введите число столбцов:");
+double [,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
+
+Console.WriteLine("Среднее арифметическое по каждому столбцу :");
+for (int j = 0 ; j < matrix.GetLength(1); j++)
+ {
+  int count = 0;
+  double sum = 0;
+  double AverageNumbers = 0;
+  for (int i = 0 ; i < matrix.GetLength(0); i++)
+  { 
+  count += 1; 
+  sum += matrix[i,j];
+  }
+  AverageNumbers = sum/count;
+  Console.Write(AverageNumbers + " ");
+ }
