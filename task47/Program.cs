836b103470473c6a2854aea38fc5069417943a83
Andreeey1997 <sem.romancov@mail.ru>
2022-12-26 23:00:27 +0300

    Обновлённое решение 47 задачи

--- task47/Program.cs
+++ task47/Program.cs
@@ -9,11 +9,11 @@ return Convert.ToInt32(Console.ReadLine());
 double [,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
 {
 double [,] matrix = new double [rows, columns];
- for (int i = 0; i<matrix.GetLength(0); i++)
+ for (int i = 0; i < matrix.GetLength(0); i++)
  {
-  for (int j = 0; j<matrix.GetLength(1); j++)
+  for (int j = 0; j < matrix.GetLength(1); j++)
   {
-  matrix[i,j]=rand.Next(leftRange, rightRange+1);
+  matrix[i,j] = rand.NextDouble() * (rightRange -leftRange) + leftRange;
   }
  }
  return matrix;
