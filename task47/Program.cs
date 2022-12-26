fd77349edabe9355e71ede931503111d6e323554
Andreeey1997 <sem.romancov@mail.ru>
2022-12-26 22:26:43 +0300

    Решение 47 задачи

---
+++ task47/Program.cs
@@ -0,0 +1,39 @@
+﻿var rand = new Random();
+
+int ReadInt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+
+double [,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+double [,] matrix = new double [rows, columns];
+ for (int i = 0; i<matrix.GetLength(0); i++)
+ {
+  for (int j = 0; j<matrix.GetLength(1); j++)
+  {
+  matrix[i,j]=rand.Next(leftRange, rightRange+1);
+  }
+ }
+ return matrix;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+ for (int i = 0; i<matrix.GetLength(0); i++)
+ {
+  for (int j = 0; j<matrix.GetLength(1); j++)
+  {
+  Console.Write(matrix[i,j]+ " ");
+  }
+  Console.WriteLine();
+ }
+}
+
+
+
+int rowsCount=ReadInt("Введите число строк:");
+int columnsCount=ReadInt("Введите число столбцов:");
+double[,] matrix = GetRandomMatrix(rowsCount, columnsCount);
+PrintMatrix(matrix);
