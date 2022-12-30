4b9f3096dd992c932b37a957822991f1f6e60db1
Andreeey1997 <sem.romancov@mail.ru>
2022-12-30 19:31:45 +0300

    Решение 68 задачи

---
+++ task68/Program.cs
@@ -0,0 +1,11 @@
+﻿int NumFirst = 3;
+int NumSecond = 4;
+
+int Akkerman (int m, int n)
+{
+if (m == 0) return n+1;
+else if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+else return Akkerman(m - 1, Akkerman (m, n - 1));
+}
+
+Console.Write(Akkerman(NumFirst,NumSecond));
