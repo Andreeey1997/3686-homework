673225da0f239a660b3b4937253d9c3d746aaa0e
Andreeey1997 <sem.romancov@mail.ru>
2022-12-30 19:31:19 +0300

    Решение 64 задачи

---
+++ task64/Program.cs
@@ -0,0 +1,21 @@
+﻿int NumFirst = 1;
+int NumSecond = 4;
+
+void Line(int n, int m)
+{
+    if (n > m)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+    else if (n == m)
+    {
+        Console.Write(m);
+    }
+    else
+    {
+        Console.Write(m);
+        Line(n, m - 1);
+    }
+}
+
+Line(NumFirst, NumSecond);
