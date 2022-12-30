3c62e86f5f5ca542a1ac3095eedd692a2c00f993
Andreeey1997 <sem.romancov@mail.ru>
2022-12-30 19:31:34 +0300

    Решение 66 задачи

---
+++ task66/Program.cs
@@ -0,0 +1,22 @@
+﻿int NumFirst = 1;
+int NumSecond = 20;
+
+int Line(int n, int m, int sum = 0)
+{
+    if (n > m)
+    {
+        Console.WriteLine("Первое число больше второго");
+        return 0;
+    }
+    else if (n == m)
+    {
+        return sum += m;
+    }
+    else
+    {
+        sum += m;
+        return Line(n, m - 1 , sum);
+    }
+}
+
+Console.WriteLine(Line(NumFirst, NumSecond));
